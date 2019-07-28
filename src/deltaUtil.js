3b7ef4642d956aa73f1237114e2dd99fbdf6a940
Robat Williams <robatwilliams@gmail.com>
2019-07-28 14:48:48 +0100

    Differential request support on the server

---
+++ src/deltaUtil.js
@@ -0,0 +1,43 @@
+function applyDelta(delta, str) {
+  // From https://github.com/kpdecker/jsdiff/issues/95#issuecomment-218429097
+
+  var result = str;
+  var pos = 0;
+  for (var i = 0; i < delta.length; i++) {
+    if (delta[i].added) {
+      result = insertAt(result, pos, delta[i].value);
+      pos += delta[i].count;
+    }
+    else if (delta[i].removed) {
+      result = removeAt(result, pos, delta[i].count);
+    } else {
+      pos += delta[i].count;
+    }
+  }
+  return result;
+}
+
+function insertAt(str, index, add) {
+  return [str.slice(0, index), add, str.slice(index)].join('');
+}
+
+function removeAt(str, index, count) {
+  return str.slice(0, index) + str.slice(index + count);
+}
+
+function minimizeDelta(delta) {
+  // No need to keep values for unchanged/removed blocks
+  return delta.map(change => {
+    if (change.added) {
+      return change;
+    } else {
+      const { value, ...keep } = change;
+      return keep;
+    }
+  });
+}
+
+module.exports = {
+  applyDelta,
+  minimizeDelta,
+};
--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const util = require('util');
 const compression = require('compression');
+const diff = require('diff');
 const express = require('express');
+const { applyDelta, minimizeDelta } = require('./deltaUtil');
 
+const BASE_VERSION_HEADER = 'x-differential-base-version';
+const DELTA_CONTENT_TYPE = 'application/delta+json';
 const PORT = 80;
 
 const readFile = util.promisify(fs.readFile);
@@ -15,18 +19,61 @@ app.get('/:package/:version/:file', asyncRoute(handleRequest));
 app.listen(PORT, () => console.log('Listening on port', PORT));
 
 async function handleRequest(request, response) {
-  const { package, version, file } = request.params;
-
-  // Danger: path traversal
-  const content = await readOptionalFile(`./resources/${package}/${version}/${file}`, 'utf8');
+  const { file } = request.params;
+  const content = await readPackageFile(request.params);
   if (content === undefined) {
     response.sendStatus(404);
     return;
   }
 
-  response.type(file.substring(file.lastIndexOf('.')));
   response.set('Cache-Control', 'public, max-age=31536000');
+  response.vary('Accept');
+
+  if (request.accepts(DELTA_CONTENT_TYPE)) {
+    return handleDifferentialRequest(request, response, content);
+  }
+
+  response.type(file.substring(file.lastIndexOf('.')));
   response.send(content);
+}
+
+async function handleDifferentialRequest(request, response, fullContent) {
+  // In future could support multiple, and choose the closest for minimal delta
+  const baseVersion = request.get(BASE_VERSION_HEADER);
+
+  if (baseVersion === undefined) {
+    response.status(400).send('No base version specified');
+    return;
+  }
+
+  const baseContent = await readPackageFile({ ...request.params, version: baseVersion });
+  if (baseContent === undefined) {
+    response.status(417).send('Specified base version not known');  // expectation failed
+    return;
+  }
+
+  const delta = createDelta(baseContent, fullContent);
+
+  response.set(BASE_VERSION_HEADER, baseVersion);
+  response.type(DELTA_CONTENT_TYPE);
+  response.vary(BASE_VERSION_HEADER);
+  response.send(delta);
+}
+
+function createDelta(base, other) {
+  const rawDelta = diff.diffChars(base, other);
+  const minimalDelta = minimizeDelta(rawDelta);
+
+  if (applyDelta(minimalDelta, base) !== other) {
+    throw new Error('Delta failed verification');
+  }
+
+  return minimalDelta;
+}
+
+function readPackageFile({ package, version, file }) {
+  // Danger: path traversal
+  return readOptionalFile(`./resources/${package}/${version}/${file}`, 'utf8');
 }
 
 async function readOptionalFile(...args) {
